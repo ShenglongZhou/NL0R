85a4ef52eba5f6168156be42e43e0acc42b3512e
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-01 20:10:32 +0100

    Update demonLR.m

--- NL0R/demonLR.m
+++ NL0R/demonLR.m
@@ -7,25 +7,24 @@ s    = ceil(0.05*n);
 test = 2;
 
 switch test
-    case 1  % You could input data including (data.A, data.At, data.b), e.g.,
+    case 1  % You could input data by our data generation function 
+         ExMat    = 1;
+         MatType  = {'Correlated','Weakly-Indipendent'};
+         data     = LogitRegdata(MatType{ExMat},m,n,s,0.5); 
+    case 2  % Or you could input data including (data.A, data.b), e.g.,
          I0       = randperm(m);  
          I        = I0(1:ceil(m/2)); 
          b        = ones(m,1);    
          b(I)     = 0;
          data.A   = repmat(b.*rand(m,1),1,n) + randn(m,n);
-         data.At  = data.A';    
          data.b   = b;
-         pars.lam = 0.01;
-    case 2  % Or you could input data by our data generation function 
-         ExMat    = 1;
-         MatType  = {'Correlated','Weakly-Indipendent'};
-         data     = logistic_random_data(MatType{ExMat},m,n,s,0.5); 
-         pars.lam = 1e-3;
+         pars.lam = 0.01;   
 end
  
 pars.draw = 1;
 pars.rate = (n>=1e3)*0.5+(n<1e3)*0.75;
-out       = NL0R('LR',data,n,pars);
+func      = @(x,T1,T2)LogitReg(x,T1,T2,data);
+out       = NL0R(func,n,pars);
 
 fprintf(' CPU time:      %.3fsec\n',  out.time);
 fprintf(' Logistic Loss: %5.2e\n', out.obj);
