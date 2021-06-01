de4ae75caf923099dcb57925aa8d1f7f03304996
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-01 20:09:45 +0100

    Update demonIMG.m

--- NL0R/demonIMG.m
+++ NL0R/demonIMG.m
@@ -3,21 +3,23 @@ clc; clear all ;close all; warning off
 
 test      = 1;
 if  test == 1
-    Img   = phantom(64*8);
+    Img   = phantom(64*4);
 else
     Img   = im2double(imread('brain.png')); 
     Img   = Img(:,:,1);
 end
-sigma     = 0.05;   % 0.05 0.01       
+sigma     = 0.01;   % 0.05 0.01       
 nsam      = 60;     % 40 60 
 [data.A,data.At,data.b,be,~,~,out0]...
           = getrealdata(Img,nsam,sigma,0);
-      
 n         = size(out0.I,2)^2;
+data.n    = n;      
 pars.tau  = sigma;
 pars.lam  = max(abs(data.b))^2*pars.tau/1e4;   
 pars.obj  = 2*norm(data.b-be)^2/3;
-out       = NL0R('CS',data,n,pars);   
+
+func      = @(x,T1,T2)CS(x,T1,T2,data);
+out       = NL0R(func,n,pars);   
 
 
 fprintf(' CPU time:          %.3fsec\n', out.time);
