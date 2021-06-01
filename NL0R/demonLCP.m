203da0b0d6ff9b33bb799e604233d9edda2f3cee
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-01 20:10:01 +0100

    Update demonLCP.m

--- NL0R/demonLCP.m
+++ NL0R/demonLCP.m
@@ -1,23 +1,19 @@
 % demon sparse linear complementarity problems
 clc; close all; clear all; warning off
 
-n         = 5000;
+n         = 10000;
 r         = 0.01;
 s         = ceil(r*n);
-data      = lcp_data('sdp',n,s);
+data      = LCPdata('sdp',n,s);
  
 pars.rate = 0.25*(r<0.05)+0.5*(r>=0.05);
-pars.tol  = 1e-15;
-out       = NL0R('LCP',data,n,pars); 
+pars.tol  = 1e-10;
+func      = @(x,T1,T2)LCP(x,T1,T2,data);
+out       = NL0R(func,n,pars); 
 
 fprintf(' Sample size:       %dx%d\n', n,n);
 fprintf(' CPU time:          %.3fsec\n',  out.time);
 fprintf(' Sparsity:          %.2d\n', nnz(out.sol));
 fprintf(' Objective:         %5.2e\n',  out.obj);
-if isfield(data,'xopt')
-   fprintf(' Accuracy:          %5.2e\n',...
-   norm(out.sol-data.xopt)/norm(data.xopt)); 
-   if  s<=500
-       ReoveryDisplay(data.xopt,out.sol,[900,500,500,250],1)
-   end
-end
+ReoveryDisplay(data.xopt,out.sol,[900 500 500 250],1)
+
