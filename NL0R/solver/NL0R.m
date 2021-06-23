d18187eb1df7bd61586b9a91f4ff49d741a68be8
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-23 18:11:51 +0100

    Update NL0R.m

--- NL0R/solver/NL0R.m
+++ NL0R/solver/NL0R.m
@@ -300,30 +300,6 @@ sx = zeros(n,1);
 sx(T(id(1:i))) = x0(T(id(1:i)));
 end
 
-
-% display x ---------------------------------------
-function RecoveryShow(xo,x,ind)
-
-   % figure('Renderer', 'painters', 'Position', pos)
-    stem(find(xo),xo(xo~=0),'bo-','MarkerSize',6, 'LineWidth',1),hold on
-    stem(find(x),x(x~=0),'r*:', 'MarkerSize',4, 'LineWidth',1),hold on
-    grid on, ymin= -0.1; ymax=0.2;
-    xx  = [xo; x];
-    if nnz(xx<0)>0, ymin= min(xx(xx<0))-0.1; end
-    if nnz(xx>0)>0, ymax= max(xx(xx>0))+0.1; end   
-    axis([1 length(x)  ymin  ymax])
-    if ind
-       st1   = strcat('Accuracy= ',num2str(norm(x-xo),2));
-       wrong = nnz(xo)-nnz(find(x~=0 & xo~=0)); 
-       st2   = strcat(', Sparsity= ',num2str(nnz(x)),...
-            ', Number of mis-indices= ',num2str(wrong)); 
-       title(strcat(st1,st2))
-       set(0,'DefaultAxesTitleFontWeight','normal');
-       legend('Ground-Truth', 'Recovered')
-    end
-end
-
-
 % conjugate gradient-------------------------------------------------------
 function x = my_cg(fx,b,cgtol,cgit,x)
     r = b;
