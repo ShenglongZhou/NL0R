b731e88a47b6454836d7790bbeec3e13c2078a85
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2023-08-13 15:40:51 +0800

    Update demonLCP.m

--- NL0R/demonLCP.m
+++ NL0R/demonLCP.m
@@ -15,4 +15,7 @@ fprintf(' Sample size:       %dx%d\n', n,n);
 fprintf(' CPU time:          %.3fsec\n',  out.time);
 fprintf(' Sparsity:          %.2d\n', nnz(out.sol));
 fprintf(' Objective:         %5.2e\n',  out.obj);
-RecoverShow(data.xopt,out.sol,[900,500,500,250],1)
+if  isfield(data,'xopt')
+    RecoverShow(data.xopt,out.sol,[900,500,500,250],1);
+end
+
